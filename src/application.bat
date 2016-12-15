f92b689f74c211a8af2526f0b9c40bfd5c64045c
Emmabille <e.billard@gmail.com>
2016-12-15 10:40:49 +0100

    Update application.bat

--- src/application.bat
+++ src/application.bat
@@ -1,6 +1,6 @@
 echo "version ${version}"
 echo "Copyright company - 2016"
 echo "Build ${build_hour}"
-### application.bat - 2016 - Rahma Barhoumi ###
+### application.bat - 2016 - Rahma Barhoumi, Emmanuelle Billard ###
 version = "2"
 build_hour = "11H00"
